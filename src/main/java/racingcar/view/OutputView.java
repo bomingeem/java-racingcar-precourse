a6600c7b6f30d11b10dd66fbcb3d62c7df298646
bomingeem <bomingeem@gmail.com>
2022-10-07 16:27:46 +0900

    feat: View 파일 생성

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,8 @@
+package racingcar.view;
+
+public class OutputView {
+    private static final String OUTPUT_EXECUTION_RESULT = "실행 결과";
+    private static final String OUTPUT_FINAL_WINNER = "최종 우승자";
+
+
+}
